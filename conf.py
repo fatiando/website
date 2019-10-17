b58acdb93b2cc1eb4d89721c4697d0f2fa83bce9
Leonardo Uieda <leouieda@gmail.com>
2019-10-17 13:25:43 +0100

    Use contact.fatiando.org for Slack invite
    I setup the subdomain to forward to our slack invite so we have a more
    stable domain name that we can update if we change the slack invite (or
    move to another service).

--- conf.py
+++ conf.py
@@ -103,7 +103,7 @@ html_context = {
         (
             '<i class="fab fa-slack fa-lg"></i>',
             "Slack chat group",
-            "https://join.slack.com/t/fatiando/shared_invite/enQtNzY4NDQ3ODQwNDk4LTc5MTU5OWNkNTczMDY4NjcyNjcyZTU0Y2I3MDQ0NWUwMmEzMTBkNmVjNTExMGZkMjA0YzM1OGYyMzZlMDk3YTU",
+            "http://contact.fatiando.org",
         ),
         (
             '<i class="fab fa-twitter fa-lg"></i>',
