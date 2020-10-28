83fb5307c57a79ef3a757653d2da30c934edba52
Agustina <pesce.agustina@gmail.com>
2020-10-28 10:00:46 -0300

    Update conf.py
    Co-authored-by: Leonardo Uieda <leouieda@gmail.com>

--- conf.py
+++ conf.py
@@ -63,7 +63,7 @@ html_theme_options = {
     "navbar_links": [
         ("About", "#about", True),
         ("Projects", "#projects", True),
-        ("Getting Started", "#started", True),
+        ("Getting Started", "#getting-started", True),
         ("Support", "#support", True),
         ("Contact", "#contact", True),
         ("Contribute", "#contribute", True),
