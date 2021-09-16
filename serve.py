4b748799635436e24f8d5cde3471835c78cc7081
Santiago Soler <santiago.r.soler@gmail.com>
2021-09-16 11:11:58 -0300

    Tell livereload to watch get_authors.py and ignore the authors files

--- serve.py
+++ serve.py
@@ -4,8 +4,24 @@ Use livereload to serve, build, and reload the website when files change.
 from livereload import Server, shell
 
 
+def ignore_authors(filepath):
+    projects = "pooch verde harmonica boule".split()
+    for project in projects:
+        if f"{project}-authors.md" in filepath:
+            return True
+    return False
+
+
 server = Server()
-files = ["**/**.rst", "**/**.md", "conf.py", "_static/", "_templates/", "theme/"]
+files = [
+    "**/**.rst",
+    "**/**.md",
+    "conf.py",
+    "get_authors.py",
+    "_static/",
+    "_templates/",
+    "theme/",
+]
 for filename in files:
-    server.watch(filename, "make clean html")
+    server.watch(filename, "make clean html", ignore=ignore_authors)
 server.serve(root="_build/html", host="localhost", open_url_delay=1)
